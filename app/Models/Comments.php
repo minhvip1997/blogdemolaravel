a280d1d304ba9a1bdd9d788dd547470f119bdd46
= <tnminh1997123@gmail.com>
2021-07-13 07:56:11 +0700

    set up enviroment

---
+++ app/Models/Comments.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comments extends Model
+{
+    use HasFactory;
+    protected $guarded = [];  
+    protected $table = "Comments";
+
+    public function author()
+    {
+        return $this->belongsTo('App\Models\User','from_user','id');
+    }
+
+    public function post()
+    {
+        return $this->belongsTo('App\Models\Posts','on_post','id');
+    }
+}
